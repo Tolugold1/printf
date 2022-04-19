da5c01c1859d303282b99ee710eadfaaaab264ac
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-04-19 01:02:20 -0700

    printf function

--- _printf.c
+++ _printf.c
@@ -15,9 +15,10 @@ int _printf(const char *format, ...)
 	va_start(valist, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == '\0')
+		if (format[i] == '%' && format[i + 1] == '\0')
 		{
-			break;
+			result += NULL;
+			i++;
 		}
 		if (format[i] == '%' && format[i + 1])
 		{
